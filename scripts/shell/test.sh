3158589b1258b000ca62364f50968ef1328f589d
Daniel Yoo <daniel.yoo0523@gmail.com>
2025-04-17 15:26:13 -0400

    rojo fix

--- scripts/shell/test.sh
+++ scripts/shell/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-aftman run rojo build "$1" --output dist.rbxl
+$HOME/.aftman/bin/rojo build "$1" --output dist.rbxl
 python3 scripts/python/upload_and_run_task.py dist.rbxl "$2"
 rm dist.rbxl
