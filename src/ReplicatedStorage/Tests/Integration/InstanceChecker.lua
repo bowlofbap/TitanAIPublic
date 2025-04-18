871512c7a87bb5c5c0c5a4418abab67ed55a0d77
Daniel Yoo <daniel.yoo0523@gmail.com>
2025-04-18 13:29:35 -0400

    updated the test runner to be able to be modularized

---
+++ src/ReplicatedStorage/Tests/Integration/InstanceChecker.lua
@@ -0,0 +1,56 @@
+return function()
+	describe("Instances created", function()
+		local ReplicatedStorage = game:GetService("ReplicatedStorage")
+		local ServerScriptService = game:GetService("ServerScriptService")
+		local MockedPlayer
+		local MockedData
+		local MapNodeTypes
+		local NodeInstanceFactory
+		local IdGenerator
+		local EventObserver
+		local PlayerState
+		local DeckManager
+		local CurrentInstance
+		local EchoManager
+		local MockedStageData
+		local MockedPosition
+		local dependencies
+		
+		beforeEach(function()
+			MockedPlayer = Instance.new("Part")
+			MockedData = require(ReplicatedStorage.Repos.StarterRepos.Ze)
+			MapNodeTypes = require(ReplicatedStorage.Enums.Entity.MapNodeTypes)
+			NodeInstanceFactory = require(ServerScriptService.NodeInstance.NodeInstanceFactory).new()
+			IdGenerator = require(ReplicatedStorage.Helpers.Classes.IdGenerator).new()
+			EventObserver = require(ServerScriptService.GameEntity.EventObserver).new()
+			PlayerState = require(ServerScriptService.GameEntity.PlayerState).new(MockedPlayer, MockedData)
+			DeckManager = require(ServerScriptService.GameEntity.DeckManager).new(MockedData.deck, IdGenerator)
+			EchoManager = require(ServerScriptService.GameEntity.EchoManager).new(EventObserver, IdGenerator, PlayerState, DeckManager, function()
+				return CurrentInstance
+			end)
+			MockedPosition = Vector3.new(0,0,0)
+			MockedStageData = require(ReplicatedStorage.Stages.Level1).tier1[1]
+			dependencies = {
+				mapNodeType = MapNodeTypes.REGULAR_ENEMY, 
+				robloxPlayer = MockedPlayer, 
+				playerState = PlayerState, 
+				deckManager = DeckManager,
+				deckData = DeckManager:getPlayableDeck(), 
+				echoManager = EchoManager, 
+				stageData = MockedStageData, 
+				parent = workspace, 
+				centerPosition = MockedPosition, 
+				idGenerator = IdGenerator, 
+				eventObserver = EventObserver,
+			}
+		end)
+
+		it("Confirms that GameInstance is initialized correctly", function()
+			CurrentInstance = NodeInstanceFactory:createInstance(MapNodeTypes.REGULAR_ENEMY, dependencies)
+			CurrentInstance:start()
+			expect(CurrentInstance._turnCount).to.equal(1)
+			expect(CurrentInstance._isPlaying).to.equal(true)
+			expect(CurrentInstance.isPlayerTurn).to.equal(true)
+		end)
+	end)
+end
